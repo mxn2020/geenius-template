b7de883e9a1e9a41a87ad72ad6256f5c165248ff
Mehdi Nabhani <mehdi@nabhani.de>
2026-03-11 09:16:13 +0100

    chore: compliance audit - add missing metadata and configs
    - Updated PROJECT.yaml: valid compliance category, expanded tags
    - Added .node-version (Node 20)
    - Added stop script to web/package.json (if applicable)

--- template/web/src/App.tsx
+++ template/web/src/App.tsx
@@ -1,3 +1,4 @@
+import { Toaster } from 'react-hot-toast'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Layout from './components/Layout'
 import { ErrorBoundary } from './components/ErrorBoundary'
@@ -48,6 +49,7 @@ function App() {
                     <CookieBanner />
                 </Layout>
             </BrowserRouter>
+            <Toaster position="bottom-right" />
         </ErrorBoundary>
     )
 }
